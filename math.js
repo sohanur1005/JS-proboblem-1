b7dd87f636dbc48c697be2cec4a52272ea54b5bc
sohanur24059 <sohanurrahman24059@gmail.com>
2025-09-27 21:45:10 +0600

    send this

---
+++ math.js
@@ -0,0 +1,17 @@
+// find minumum number
+
+const min=Math.min(41,7,58,2,3,45,6)
+console.log('Minimum Number ',min)
+// find maximum number
+
+const max=Math.max(41,7,58,2,3,45,6)
+console.log('Maximum Number ',max)
+
+console.log(Math.PI)
+console.log(Math.abs(-10))  
+console.log(Math.round(2.8)) 
+console.log(Math.round(2.2)) 
+// floor cut the point value
+console.log(Math.floor(2.8))
+// ceiling add point value and make this non fraction value
+console.log(Math.ceil(2.0001)) 
